2829ebe2f0fc69e6f77f01515e102863b0982faf
Ilan Olkies <ilanolkies@outlook.com>
2019-03-21 13:01:40 -0300

    MultiChainResolver support PublicResolver addr

--- contracts/resolver/MultiChainResolver.sol
+++ contracts/resolver/MultiChainResolver.sol
@@ -1,3 +1,15 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-contract MultiChainResolver { }
+import "./AbstractAddrResolver.sol";
+
+contract MultiChainResolver {
+    AbstractAddrResolver publicResolver;
+
+    constructor (AbstractAddrResolver _publicResolver) public {
+        publicResolver = _publicResolver;
+    }
+
+    function addr(bytes32 node) public view returns (address) {
+        return publicResolver.addr(node);
+    }
+}
--- test/multi_chain_resolver.js
+++ test/multi_chain_resolver.js
@@ -1,11 +1,33 @@
+const assert = require('assert');
 const RNS = artifacts.require('RNS');
+const PublicResolver = artifacts.require('PublicResolver');
 const MultiChainResolver = artifacts.require('MultiChainResolver');
+const namehash = require('eth-ens-namehash').hash;
 
-contract('MultiChainResolver', async () => {
+contract('MultiChainResolver', async (accounts) => {
+  var publicResolver, multiChainResolver;
+
+  const hash = namehash('rsk');
+
   beforeEach(async () => {
-    await RNS.new();
-    await MultiChainResolver.new();
+    const rns = await RNS.new();
+    publicResolver = await PublicResolver.new(rns.address);
+
+    await rns.setSubnodeOwner(0, web3.sha3('rsk'), accounts[0]);
+    await rns.setResolver(hash, publicResolver.address);
+
+    multiChainResolver = await MultiChainResolver.new(publicResolver.address);
   });
 
   it('should create smart contracts', async () => { return });
+
+  it('should return public resolver address', async () => {
+    const addr = '0x0000000000111111111122222222223333333333';
+
+    await publicResolver.setAddr(hash, addr);
+
+    const actualAddr = await multiChainResolver.addr(hash);
+
+    assert.equal(actualAddr, addr);
+  });
 });
