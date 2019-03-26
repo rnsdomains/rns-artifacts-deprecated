48870a0dac8d44b37fb066f8443637e21cb2f045
Ilan Olkies <ilanolkies@outlook.com>
2019-03-26 13:24:33 -0300

    Whitelist allows owner to set managers

--- contracts/util/Whitelist.sol
+++ contracts/util/Whitelist.sol
@@ -3,7 +3,18 @@ pragma solidity >=0.4.21 <0.6.0;
 contract Whitelist {
 	address public owner;
 
+	mapping (address => bool) public isManager;
+
+	modifier isOwner () {
+		require(msg.sender == owner);
+		_;
+	}
+
 	constructor () public {
 		owner = msg.sender;
+	}
+
+	function addManager (address manager) public isOwner() {
+		isManager[manager] = true;
 	}
 }
--- test/util/whitelist.test.js
+++ test/util/whitelist.test.js
@@ -17,4 +17,32 @@ contract('Whitelist', async accounts => {
 
     assert.equal(owner, accounts[0]);
   });
+
+  it('should be able to sotre managers', async () => {
+    const manager1 = accounts[1];
+    const manager2 = accounts[2];
+
+    await whitelist.addManager(manager1);
+    await whitelist.addManager(manager2);
+
+    const isManager1 = await whitelist.isManager(manager1);
+    const isManager2 = await whitelist.isManager(manager2);
+
+    assert.ok(isManager1);
+    assert.ok(isManager2);
+  });
+
+  it('should let only owner to add managers', async () => {
+    const manager = accounts[1];
+
+    try {
+      await whitelist.addManager(manager, { from: accounts[1] });
+    } catch {
+      const isManager = await whitelist.isManager(accounts[1]);
+      assert.ok(!isManager);
+      return;
+    }
+
+    assert.fail();
+  });
 });
