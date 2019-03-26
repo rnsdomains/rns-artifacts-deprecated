eeb5f9595d126dfa7378a05e34d85b947a82b63f
Ilan Olkies <ilanolkies@outlook.com>
2019-03-25 21:53:07 -0300

    PublicSubdomainRegistrar stores rns address

--- contracts/registrar/PublicSubdomainRegistrar.sol
+++ contracts/registrar/PublicSubdomainRegistrar.sol
@@ -1,5 +1,11 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-contract PublicSubdomainRegistrar {
+import "../registry/AbstractRNS.sol";
 
+contract PublicSubdomainRegistrar {
+    AbstractRNS public rns;
+
+    constructor (AbstractRNS _rns) public {
+        rns = _rns;
+    }
 }
--- test/registrar/public_subdomain_registrar.test.js
+++ test/registrar/public_subdomain_registrar.test.js
@@ -1,7 +1,20 @@
+const assert = require('assert');
 const PublicSubdomainRegistrar = artifacts.require('PublicSubdomainRegistrar');
+const RNS = artifacts.require('RNS');
 
 contract('PublicSubdomainRegistrar', async () => {
-  it('should create PublicSubdomainRegistrar contract', async () => {
-    await PublicSubdomainRegistrar.new();
+  var publicSubdomainRegistrar, rns;
+
+  beforeEach(async () => {
+    rns = await RNS.new();
+    publicSubdomainRegistrar = await PublicSubdomainRegistrar.new(rns.address);
+  });
+
+  it('should create PublicSubdomainRegistrar contract', async () => { return });
+
+  it('should store RNS address', async () => {
+    const rnsAddress = await publicSubdomainRegistrar.rns();
+
+    assert.equal(rnsAddress, rns.address);
   });
 });
