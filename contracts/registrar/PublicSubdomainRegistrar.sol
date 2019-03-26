bd5cfac0e436f1609f1851fd58a157defa04ab17
Ilan Olkies <ilanolkies@outlook.com>
2019-03-25 22:13:02 -0300

    PublicSubdomainRegistrar accepts only owned delegated nodes

--- contracts/registrar/PublicSubdomainRegistrar.sol
+++ contracts/registrar/PublicSubdomainRegistrar.sol
@@ -5,7 +5,14 @@ import "../registry/AbstractRNS.sol";
 contract PublicSubdomainRegistrar {
     AbstractRNS public rns;
 
+    modifier onlyOwned (bytes32 node) {
+        require(rns.owner(node) == address(this));
+        _;
+    }
+
     constructor (AbstractRNS _rns) public {
         rns = _rns;
     }
+
+    function delegate (bytes32 node) public onlyOwned(node) {}
 }
--- test/registrar/public_subdomain_registrar.test.js
+++ test/registrar/public_subdomain_registrar.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const PublicSubdomainRegistrar = artifacts.require('PublicSubdomainRegistrar');
 const RNS = artifacts.require('RNS');
+const namehash = require('eth-ens-namehash').hash;
 
 contract('PublicSubdomainRegistrar', async () => {
   var publicSubdomainRegistrar, rns;
@@ -16,5 +17,20 @@ contract('PublicSubdomainRegistrar', async () => {
     const rnsAddress = await publicSubdomainRegistrar.rns();
 
     assert.equal(rnsAddress, rns.address);
+  });
+
+  it('should receive delegated root nodes only if owned', async () => {
+    const hash = namehash('rsk');
+    const label = web3.sha3('rsk');
+
+    try {
+      await publicSubdomainRegistrar.delegate(hash);
+    } catch {
+      await rns.setSubnodeOwner(0, label, publicSubdomainRegistrar.address);
+      await publicSubdomainRegistrar.delegate(hash);
+      return;
+    }
+
+    assert.fail();
   });
 });
