f7098d0dedf0076e017e91ad19f2cf79b09830e2
Ilan Olkies <ilanolkies@outlook.com>
2019-03-26 13:14:48 -0300

    Whitelist stores an owner

--- test/util/whitelist.test.js
+++ test/util/whitelist.test.js
@@ -1,9 +1,20 @@
 const assert = require('assert');
 const Whitelist = artifacts.require('Whitelist');
 
-contract('Whitelist', async () => {
+contract('Whitelist', async accounts => {
+  var whitelist;
+
+  beforeEach(async () => {
+    whitelist = await Whitelist.new();
+  });
+
   it('should create Whitelist contract', async () => {
-    const whitelist = await Whitelist.new();
     assert(whitelist.address);
+   });
+
+  it('should store deployer as owner', async () => {
+    const owner = await whitelist.owner();
+
+    assert.equal(owner, accounts[0]);
   });
 });
