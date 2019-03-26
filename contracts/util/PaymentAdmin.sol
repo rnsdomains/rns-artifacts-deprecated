6e2a8682829a9dd9e673539d6d926398ae6cd08d
Ilan Olkies <ilanolkies@outlook.com>
2019-03-26 16:07:00 -0300

    PaymentAdmin stores owner address

--- contracts/util/PaymentAdmin.sol
+++ contracts/util/PaymentAdmin.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract PaymentAdmin {
-
+    address public owner = msg.sender;
 }
--- test/util/payment_admin.js
+++ test/util/payment_admin.js
@@ -1,14 +1,21 @@
 const assert = require('assert');
 const PaymentAdmin = artifacts.require('PaymentAdmin');
 
-contract('PaymentAdmin', async () => {
+contract('PaymentAdmin', async accounts => {
   var paymentAdmin;
+  const owner = accounts[0];
 
   beforeEach(async () => {
-    paymentAdmin = await PaymentAdmin.new();
+    paymentAdmin = await PaymentAdmin.new({ from: owner });
   });
 
   it('should create PaymentAdmin contract', async () => {
     assert.ok(paymentAdmin.address);
+  });
+
+  it('should store deployer as owner', async () => {
+    const actualOwner = await paymentAdmin.owner();
+
+    assert.equal(actualOwner, owner);
   });
 });
