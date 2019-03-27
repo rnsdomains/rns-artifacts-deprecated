f0032b4e3e78a67660ebe43904fd8679d3dbfd6e
Ilan Olkies <ilanolkies@outlook.com>
2019-03-27 14:35:53 -0300

    PaymentAdmin transfer tokens given an amount

--- contracts/util/PaymentAdmin.sol
+++ contracts/util/PaymentAdmin.sol
@@ -5,12 +5,21 @@ import "../token/ERC20Basic.sol";
 contract PaymentAdmin {
     address public owner = msg.sender;
 
+    modifier onlyOwner () {
+        require(msg.sender == owner);
+        _;
+    }
+
     function () external payable {
         owner.transfer(msg.value);
     }
 
-    function retriveTokens (ERC20Basic token, address receiver) public {
+    function retriveTokens (address receiver, ERC20Basic token) public onlyOwner() {
         uint256 balance = token.balanceOf(address(this));
         token.transfer(receiver, balance);
+    }
+
+    function transfer (address receiver, ERC20Basic token, uint256 value) public onlyOwner() {
+        token.transfer(receiver, value);
     }
 }
