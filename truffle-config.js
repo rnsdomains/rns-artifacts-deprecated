5a807f371c251f72c227a6ce53f06eaac93a533e
Ilan Olkies <ilanolkies@outlook.com>
2019-06-21 12:44:28 -0300

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 8545,
-      network_id: "*"
+      network_id: '*'
     }
   },
   compilers: {
     solc: {
-      version: "^0.5.2"
+      version: '^0.5.2'
     }
   }
-}
+};
